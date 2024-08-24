7e19729fd005f937a20fbe4717b1e07902682a62
npng6746 <npng6746@uni.sydney.edu.au>
2024-08-24 21:41:29 +1000

    post for suggestions

--- backend/routes/places.js
+++ backend/routes/places.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const router = require('express').Router()
 require('dotenv').config();
 module.exports = router
-const key = 'key';
+const key = 'AIzaSyB_XZ2Q20yGdz-7wWgel1EaNv9Pw6vzgeY';
 router.get('/restaurants', async (req, res, next) => {
  try {
    const neighborhood = 'chelsea'
@@ -22,12 +22,9 @@ router.get('/restaurants', async (req, res, next) => {
 
 router.get('/suggestions', async (req, res, next) => {
     try {
-      const center = { lat: 52.369358, lng: 4.889258 };
-      const radius = 500;
-      const type = 'restaurant';
-      const maxResults = 5;
-      const language = 'en-US';
-      const region = 'us';
+
+      const center = { lat: -33.891602606625575, lng: 151.1912928921235 };
+      
   
       const { data } = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
         params: {
@@ -47,6 +44,47 @@ router.get('/suggestions', async (req, res, next) => {
         businessStatus: place.business_status,
       }));
   
+      res.json(places);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+
+  router.post('/suggestions', async (req, res, next) => {
+    try {
+      // Destructure the parameters from the request body
+      const locations = req.body.locations;
+      const [location1, location2] = locations;
+    const latMid = (location1.lat + location2.lat) / 2;
+    const lngMid = (location1.lng + location2.lng) / 2;
+
+
+      const radius = 5000;
+      const type = 'natural_feature';
+      const maxResults = 5;
+      const language = 'en-US';
+      const region = 'us';
+  
+      const { data } = await axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json', {
+        params: {
+          location: `${latMid},${lngMid}`,
+          radius: radius,
+          type: type,
+          language: language,
+          region: region,
+          key: key,
+          rankby: 'prominence', // To mimic rankPreference: SearchNearbyRankPreference.POPULARITY
+        },
+      });
+  
+      const places = data.results.slice(0, maxResults).map(place => ({
+        displayName: place.name,
+        location: place.geometry.location,
+        businessStatus: place.business_status,
+      }));
+      
+      console.log(places)
       res.json(places);
     } catch (err) {
       next(err);
