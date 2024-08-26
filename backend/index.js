1de0d6eb739a86cde099f33f06ee3511e36f376e
Leon Lee <leonlee@sydney.edu.au>
2024-08-26 21:32:13 +1000

    added frontend and backend integration

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
+const fs = require('fs');
+
 
 
 const app = express()
+
+if (fs.existsSync(path.join(__dirname, 'dist'))) {
+    app.use(express.static(path.join(__dirname, 'dist')));
+}
+
 app.use(cors())
 
 app.use(express.json())
